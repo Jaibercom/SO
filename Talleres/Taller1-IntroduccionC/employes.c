72d28e84e0b775936128cec90915b543fb8f9b98
jaibercom <jaibercom@gmail.com>
2016-09-09 06:26:52 -0700

    taller1 ude@

--- Talleres/Taller1-IntroduccionC/employes.c
+++ Talleres/Taller1-IntroduccionC/employes.c
@@ -1,118 +1,108 @@
+/*
+	Descripcion: taller1 realizado ude@ 
+	autor: Jaiber Yepes 
+	fecha: 09 Sept 2016
+
+	Pendiente: -modificar la función "showEmployees" para hacer una salida formateada acorde a la descripción del taller 
+			   -Crear una función que calcule el salario total de todos los trabajadores
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_CHARS_NAME 20
+#define MAX_CHARS_NAME 50
+#define EMP_MAX 10
 
+//Declaracion de funciones
 void menu(char *action);
-//void addEmployees(char (*names)[MAX_CHARS_NAME], float *salaries, int *emp_count, int emp_max);
-//void showEmployees();
-float calculateAux(float salary, int percentage);
+void addEmployees(char (*ptr_names)[MAX_CHARS_NAME], float *ptr_salaries, int *count);
+void showEmployees(char (*ptr_names)[MAX_CHARS_NAME], float *salaries, int *count);
+
+//int emp_max =10;
+
 
 int main(int argc, char *argv[]) {
-	int emp_max;
-	int emp_count = 0;
+	
+	int emp_count = 0;		//numero de empleados 
 	char action = 'x';
 	int i;
+	float salaries[EMP_MAX];
+	char names[EMP_MAX][MAX_CHARS_NAME];
 
-	char (*names)[MAX_CHARS_NAME];
-	float *salaries;
+	printf("numero max de empleados es : %d\n", EMP_MAX);
+	
+	do {
+		menu(&action);
 
-	/*if (argc != 2) {
-		printf("Error, la ejecución del programa debe ser: %s <num_max_trabajadores>", argv[0]);
-		return 1;
+		switch(action) {
+		  case 'a':
+				printf("\n**Registrar trabajadores**\n"); 
+				addEmployees( names, salaries, &emp_count);
+			break;
+		  case 'b':
+				printf("**Ver trabajadores**\n"); 
+				showEmployees( names, salaries, &emp_count);
+			break;
+		  case 'q':
+				puts("Saliendo del programa...");
+			break;
+		  default:
+				puts("Opción no valida, intenta de nuevo.");
+			break;
+		}
+	} while(action != 'q');
+
+	return 0;
+}
+
+//Implementacion de funciones
+void menu(char *ptr_action) {
+	
+	puts("MENÚ DE OPCIONES");
+	printf("a) Registrar trabajadores\n");
+	printf("b) Ver lista de trabajadores\n");
+	printf("q) Salir del programa\nSeleccione una opción:");
+
+	setbuf(stdin, NULL);
+	scanf("\n%c", ptr_action);
+
+}
+
+void addEmployees(char (*ptr_names)[MAX_CHARS_NAME],float *ptr_salaries, int *count){
+	int num = 0;
+	int i = 0;
+	float aux;
+	//float *ptr = &sal;
+	//*ptr = 45.4;
+
+	printf("Ingrese el número de empleados a registrar: ");
+	setbuf(stdin, NULL);
+	scanf("%d", &num);
+
+	if ((*count + num) > EMP_MAX) {
+		printf("El número de empreados a registrar sobrepasa la memoria.\n");
+		return;
 	}
+	
+	for(i=*count; i< (*count + num) ; i++){
+		
+		printf("ingrese el nombre %d  \n", i+1);
+		scanf("%s", (char *)(ptr_names+i));
 
-	emp_max = atoi(argv[1]);
-*/
-	for(i=0; i<argc; i++){
-		printf("%s\n", argv[i]);
+		printf("ingrese el salario %d  \n", i+1);
+		//scanf("%f", &aux);
+		//*(ptr_salaries+i) = aux;	
+		scanf("%f", ptr_salaries+i);
 	}
-
-
-/*
-  do {
-    menu(&action);
-
-    switch(action) {
-      case 'a':
-        
-        break;
-      case 'b':
-        
-        break;
-      case 'c':
-        
-        break;
-      case 'q':
-        puts("Saliendo del programa...");
-        break;
-      default:
-        puts("Opción no valida, intenta de nuevo.");
-        break;
-    }
-  } while(action != 'q');
-*/
-  return 0;
+	//count = *count + num; 
+	*count += num;
 }
 
-void menu(char *action) {
-  puts("MENÚ DE OPCIONES");
-  printf("a) Registrar trabajadores\n");
-  printf("b) Ver lista de trabajadores\n");
-  printf("c) Ver nómina\nq) Salir del programa\nSeleccione una opción:");
-
-  setbuf(stdin, NULL);
-  scanf("\n%c", action);
-}
-/*
-void addEmployees(char (*names)[MAX_CHARS_NAME], float *salaries, int *emp_count, int emp_max) {
-  int n = 0;
-  int i = 0;
-
-  printf("Ingrese el número de empleados a registrar: ");
-
-  setbuf(stdin, NULL);
-  scanf("%d", &n);
-
-  if (*emp_count + n > emp_max) {
-    printf("El número de empreados a registrar sobrepasa la memoria.");
-    return;
-  }
-
-  for (i = *emp_count; i < *emp_count + n; i++) {
-   
-  }
-
-  *emp_count = *emp_count + n;
-}
-/*
-void showEmployees(char (*names)[MAX_CHARS_NAME], float *salaries, int emp_count) {
-  int i;
-
-  printf("%-50s|%12s\n", "Nombre del trabajador", "Salario");
-
-  for (i = 0; i < emp_count; i++) {
-   
-  }
+void showEmployees(char (*ptr_names)[MAX_CHARS_NAME], float *ptr_salaries, int *count){
+	int i = 0;
+	for(i=0; i<(*count); i++){
+		printf("Nombre %d = %s\n", i+1, (char *)(ptr_names+i));
+		printf("salario %d = %.1f\n", i+1, *(ptr_salaries+i));		
+	}
 }
 
-void showPayroll(char (*names)[MAX_CHARS_NAME], float *salaries, int emp_count) {
-  int i;
-  float aux;
-  float salary;
-  float total = 0;
-  float subtotal = 0;
-  float percentage = 10;
-
-  printf("%-50s|%10s|%12s|%12s\n", "Nombre del trabajador", "Salario", "Aux. Trans.", "Total");
-
-  for (i = 0; i < emp_count; i++) {
-    
-  }
-
-  printf("%75s%12.2f\n", "total", total);
-}
-
-float calculateAux(float salary, int percentage) {
-  return (salary * percentage / 100);
-}*/
