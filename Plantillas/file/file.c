599966ffce65b1a98a8a21dff1760cb129661cf2
Santiago Osorio <santiagoosorio1@gmail.com>
2016-09-08 20:26:16 -0500

    Updated file reader template

--- Plantillas/file/file.c
+++ Plantillas/file/file.c
@@ -9,49 +9,79 @@ typedef struct {
   int quantity;
 } structure_name;
 
-void lineProcess(char *line, char *sep, structure_name *my_structure);
+void validateArgs(int argc, char *argv[]);
+FILE *openFile(char *filename, char *mode);
+void readFile(FILE *file, structure_name *my_structure);
+void lineProcess(char *line, structure_name *my_structure);
+void printData(structure_name *my_structure);
 
 int main (int argc, char *argv[]) {
-  int i = 0;
-  char sep;
-  char line[MAX];
-  char *fileName;
   FILE *file;
   structure_name *my_structure;
 
-  if (argc != 2) {
-    puts('Error al ejecutar, debes hacerlo de la siguiente forma');
-    printf('%s archivo.csv', argv[0]);
-  }
+  validateArgs(argc, argv);
 
-  sep = ',';
-  fileName = argv[1];
-
-  file = fopen(fileName, "r");
+  file = openFile(argv[1], "r");
 
   my_structure = malloc(sizeof(my_structure) * 2);
 
-  while (!feof(file)) {
-    fgets(line, MAX, file);
-    lineProcess(line, &sep, my_structure + i);
+  readFile(file, my_structure);
 
-    printf("%s\n", (my_structure + i)->name);
-    printf("%d\n", (my_structure + i)->quantity);
-
-    i++;
-  }
+  printData(my_structure);
 
   fclose(file);
 
   return 0;
 }
 
-void lineProcess(char *line, char *sep, structure_name *my_structure) {
+void validateArgs(int argc, char *argv[]) {
+  if (argc != 2) {
+    puts("Error al ejecutar, debes hacerlo de la siguiente forma");
+    printf("%s data.csv\n", argv[0]);
+    exit(1);
+  }
+}
+
+FILE *openFile(char *filename, char *mode) {
+  FILE *file;
+
+  file = fopen(filename, mode);
+
+  if (!file) {
+    printf("Error al abrir el archivo: %s\n", filename);
+    exit(1);
+  }
+
+  return file;
+}
+
+void readFile(FILE *file, structure_name *my_structure) {
+  int i = 0;
+  char line[MAX];
+
+  while (fgets(line, MAX, file)) {
+    lineProcess(line, my_structure + i);
+
+    i++;
+  }
+}
+
+void lineProcess(char *line, structure_name *my_structure) {
   char *pch;
+  char sep[] = ",";
 
   pch = strtok(line, sep);
   strcpy(my_structure->name, pch);
 
   pch = strtok (NULL, sep);
   my_structure->quantity = atoi(pch);
+}
+
+void printData(structure_name *my_structure) {
+  int i;
+
+  for (i = 0; i < 2; i++) {
+    printf("Nombre: %s\n", (my_structure + i)->name);
+    printf("Cantidad: %d\n", (my_structure + i)->quantity);
+  }
 }
