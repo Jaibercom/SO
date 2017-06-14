81b55b02fb277153b2b800cb205f92fdea3411ad
jaibercom <jaibercom@gmail.com>
2017-06-14 15:29:37 -0500

    Updating

--- Talleres20171/Taller1/mares.c
+++ Talleres20171/Taller1/mares.c
@@ -27,8 +27,6 @@ int getNumberSubjets(FILE *file);
 void readData(FILE *file, int numSubjets, strSubjet *prtStrSubjet);
 void printData(FILE *file, int numSubjets, strSubjet *prtStrSubjet);
 float calculateAverage(int numSubjets, strSubjet *prtStrSubjet);
-void printResult(/* implementar*/);
-
 
 int main(int argc, char *argv[]){
 	
@@ -66,13 +64,9 @@ int main(int argc, char *argv[]){
 	readData(inFile, numSubjets, subjets); 
 	printData(inFile, numSubjets, subjets); 
 	
-
 	//Realizar calculos
 	average = calculateAverage(numSubjets, subjets);
 	printf("\nEl promedio ponderado es : %0.1f\n", average);
-	fprintf(inFile, "\nEl promedio ponderado es : %0.1f\n", average);
-	/*printResult();
-	*/
 
 	//Liberar memoria
 	fclose(inFile);
@@ -112,19 +106,20 @@ void readData(FILE *file, int numSubjets, strSubjet *prtStrSubjet){
 	for(i=0; i<numSubjets; i++){
 
 		fgets(buffer, 100, file);
+		//puts(buffer);
 		
 		//"Laboratorio;4.5;1"     "4.5"
 		pch = (char *)strtok(buffer, ";");
 		strcpy(prtStrSubjet->name, pch);
-		//printf("**%s\n", prtStrSubjet->name);
+		//printf("*Materia: %s\n", prtStrSubjet->name);
 
 		pch = (char *)strtok(NULL, ";");
 		prtStrSubjet->grade = atof(pch);
-		//printf("**float: %.1f\n", prtStrSubjet->grade);
+		//printf("*Nota: %.1f\n", prtStrSubjet->grade);
 
 		pch = (char *)strtok(NULL, ";");
 		prtStrSubjet->credits = atoi(pch);
-		//printf("**entero %d\n", prtStrSubjet->credits);
+		//printf("*Creditos %d\n", prtStrSubjet->credits);
 
 		prtStrSubjet++;
 		
@@ -137,12 +132,10 @@ void printData(FILE *file, int numSubjets, strSubjet *prtStrSubjet){
 	char *pch;
 	char buffer[100];
 
+	printf("%-20.20s -- %-6.6s -- %-8.8s\n", "Materia", "Nota", "Creditos");
 	for(i=0; i<numSubjets; i++){
 
-		printf("%s\n", prtStrSubjet->name);
-		printf("float: %.1f\n", prtStrSubjet->grade);
-		printf("entero %d\n", prtStrSubjet->credits);
-
+		printf("%-20.18s -- %-6.1f --    %-8d\n", prtStrSubjet->name, prtStrSubjet->grade, prtStrSubjet->credits);
 		prtStrSubjet++;
 		
 	}
@@ -153,7 +146,7 @@ float calculateAverage(int numSubjets, strSubjet *prtStrSubjet){
 	int i = 0;
 	float avg = 0;
 	int numCred = 0;
-	float numGrades;
+	float numGrades = 0;
 
 	for(i=0; i<numSubjets; i++){
 		numCred += prtStrSubjet->credits;
@@ -161,20 +154,10 @@ float calculateAverage(int numSubjets, strSubjet *prtStrSubjet){
 
 		prtStrSubjet++;
 	}
+	//printf("numGrades: %.1f\n", numGrades);	
 	//printf("numCred: %d\n", numCred);
-	//printf("numGrades: %.1f\n", numGrades);
 
 	avg = numGrades/numCred;
 	return avg;
 }
 
-void printResult(){
-	//Implementar, escribir en fichero de salida 
-	/*
-	int i = 0;
-	printf("\n%-10s %-5s %-8s\n", "Nombre", "Nota", "Creditos" );
-
-
-	//printf("El promedio ponderado es: %.1f \n", _average);
-	*/
-}
