f79bb24dd8365d2e397f072ced58b1d96b68b1c5
Jaiber <jaibercom@gmail.com>
2018-02-13 15:11:08 -0500

    Updating

--- Ejemplos/DynamicAllocating/memoryAllocation.c
+++ Ejemplos/DynamicAllocating/memoryAllocation.c
@@ -11,6 +11,7 @@
 
 //Declaracion de funciones 
 void getData(float * , int );
+float *reserveDynamicMemory(int n);
 void printData(float * , int );
 float average(float * , int );
 
@@ -21,11 +22,11 @@ int main(){
 	float *numbers;
 	float avg;
 	int size; 
-	
+
 	printf("Ingrese el numero de datos \n");
 	scanf("%d", &size);
-	numbers = (float *) malloc(sizeof(float)*size);
-	
+	numbers = reserveDynamicMemory(size);
+
 	getData(numbers, size);
 	printData(numbers, size);
 	avg = average(numbers, size);
@@ -38,26 +39,46 @@ int main(){
 }
 
 //Implementacion de funciones 
+float *reserveDynamicMemory(int n){
+
+	float *ptr = NULL;
+	//printf("n = %d\n", *n);
+	ptr = (float *) malloc(n*sizeof(float));
+	
+	return ptr;
+}
+
+
 void getData(float *ptrNumbers, int size){
+
 	int i;
+
 	printf("Ingrese %d datos: \n", size);
 	for(i=0; i<size; i++){
-		scanf("%f", ptrNumbers++);		//scanf("%f", numbers);
+		scanf("%f", ptrNumbers++);		
+		//scanf("%f", numbers);
 		//scanf("%f", &ptrNumbers[i]);
 		//ptrNumbers++;	
 	}
 }
 
+
 void printData(float *ptrNumbers , int size){
+
 	int i;
-	for(i=0; i<size; i++){
+	printf("Los datos son: ");
+
+	for(i=0; i<size-1; i++){
 		//printf("Los datos son: %.1f\n", numbers[i]);
-		printf("Los datos son: %.1f\n", *ptrNumbers);
+		printf("%.1f, ", *ptrNumbers);
 		ptrNumbers++;
 	}
+
+	printf("%.1f \n", *ptrNumbers);
 }
 
 float average(float *ptrNumbers , int size){
+
 	float avg = 0;
 	float sum = 0;
 	int i;
