f2d1a1340c8a0ca3a4637787cbb32cba578f2171
jaiber <jaibercom@gmail.com>
2018-04-12 07:48:44 -0500

    Philosopher problem

--- Synchronization/Problems/Filosofos.c
+++ Synchronization/Problems/Filosofos.c
@@ -1,7 +1,7 @@
 /*
-Cena de los filosofos con semáforos.
-Durante el tiempo que un filósofo está comiendo o pensando permanecerá 
-bloqueado un número aleatorio de segundos entre 0 y 10.
+	Cena de los filosofos con semáforos.
+	Durante el tiempo que un filósofo está comiendo o pensando permanecerá 
+	bloqueado un número aleatorio de segundos entre 0 y 10.
 */
 
 #include <stdio.h>
@@ -10,10 +10,10 @@ bloqueado un número aleatorio de segundos entre 0 y 10.
 #include <semaphore.h>
 #include <unistd.h>
  
-#define N 5 //Numero de filósofos
+#define N 5 			// Numero de filósofos
 #define VIDA 10
 #define IZQUIERDA(x)	((x) !=0 ? (x)-1 : (N-1)) //Calcula el filosofo de la izqda
-#define DERECHA(x)		(((x)+1)%N) //Calcula el filosofo de la dcha
+#define DERECHA(x)		(((x)+1)%N) 	//Calcula el filosofo de la dcha
 
 typedef enum{
 	PENSANDO,HAMBRIENTO,COMIENDO
@@ -25,36 +25,30 @@ sem_t mutex,s[N]; //Mutex: Solo un filosofo puede coger o soltar tenedores; s[N]
 void pensar(int id);
 void comer(int id);
 void coger_tenedores(int id);
-void Comprobar(int id);
+void comprobar(int id);
 void soltar_tenedores(int id);
+void *Filosofo(void* );
 
-int main()
-{
-	extern status estado[N]; //No es necesario
-	extern sem_t mutex,s[N]; //No es necesario
+int main(){
 
-	int v[N]/*para decirle a cada hilo quien es*/,value,i;
+	int v[N]/*para decirle a cada hilo quien es*/; 
+	int value,i;
 	sem_init(&mutex,0,1);
 	pthread_t filosofo[N];
-	void *Filosofo(void* );
 	
-	for(i=0;i<N;i++)				//NO SE PUEDEN LANZAR LOS HILOS HASTA QUE LOS SEMAFOROS NO ESTEN INICIALIZADOS
-	{
+	for(i=0;i<N;i++){
 		sem_init(&s[i],0,0);
 		estado[i]=PENSANDO;
-
 	}
 
-	for(i=0;i<N;i++)
-	{
-		v[i]=i;
+	for(i=0;i<N;i++){
+		v[i]=i+1;
 		if(value=pthread_create(&filosofo[i],NULL,Filosofo, (void *) &v[i]))
 			exit(value);
 
 	}
 	
-	for(i=0;i<N;i++)
-	{
+	for(i=0;i<N;i++){
 		pthread_join(filosofo[i],NULL);
 	}
 
@@ -66,8 +60,7 @@ void *Filosofo(void* i)
 	int id,j;
 	id=*(int *) i;
 
-	for(j=0;j<2;j++)
-	{
+	for(j=0;j<2;j++){
 		pensar(id);
 		coger_tenedores(id);
 		comer(id);
@@ -77,13 +70,13 @@ void *Filosofo(void* i)
 
 void pensar(int id)
 {
-	printf("Filosofo %d pensando\n",id);
+	printf("Filosofo %d: pensando\n",id);
 	sleep (random() % 10);
 }
 
 void comer(int id)
 {
-	printf("Filosofo %d comiendo\n",id);
+	printf("Filosofo %d: comiendo\n",id);
 	sleep (random() % 10);
 }
 
@@ -91,17 +84,17 @@ void coger_tenedores(int id)
 {
 	sem_wait(&mutex); //Si un filosofo esta cogiendo tenedores, no podre coger y me quedare bloqueado. Si no hay nadie cogiendo tenedores, cogere yo
 	estado[id]=HAMBRIENTO;
-	Comprobar(id);
+	comprobar(id);
 	sem_post(&mutex); //Permito que otro filosofo coja tenedores
-	sem_wait(&s[id]); //Si no hace el post de la funcion Comprobar(), se quedaria pillado aqui
+	sem_wait(&s[id]); //Si no hace el post de la funcion comprobar(), se quedaria estancado aqui
 }
 
-void Comprobar(int id)
+void comprobar(int id)
 {
 	if(estado[id]==HAMBRIENTO && estado[IZQUIERDA(id)]!=COMIENDO && estado[DERECHA(id)]!=COMIENDO)
 	{
 		estado[id]=COMIENDO;
-		sem_post(&s[id]); //Sirve para liberar al filosofo para que no se quede pillado mas adelante, puesto que ha podido coger tenedores
+		sem_post(&s[id]); //Sirve para liberar al filosofo para que no se quede estancado mas adelante, puesto que ha podido coger tenedores
 	}
 }
 
@@ -109,7 +102,7 @@ void soltar_tenedores(int id)
 {
 	sem_wait(&mutex); //Si hay alguien cogiendo tenedores, nadie mas puede coger. Si estoy cogiendo tenedores, bloqueo para que nadie mas pueda hacerlo 
 	estado[id]=PENSANDO;
-	Comprobar(IZQUIERDA(id));
-	Comprobar(DERECHA(id));
+	comprobar(IZQUIERDA(id));
+	comprobar(DERECHA(id));
 	sem_post(&mutex);
 }
